8a9add02ec50c4ee1918eafbe592da708feabd71
Ivan Gil <igil.dev@gmail.com>
2018-11-15 21:45:08 -0500

    Navigation issues fixed and new items added

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,7 +4,7 @@ const Product = require('../models/product');
 
 exports.productsData = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop/product-list', {
+    res.render('shop/products-list', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
@@ -18,9 +18,14 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout.ejs', { pageTitle: 'Checkout', path: '/checkout' });
+  res.render('shop/checkout', { pageTitle: 'Checkout', path: '/checkout' });
 };
 
 exports.getIndex = (req, res, next) =>{
   res.render('shop/index', {pageTitle: 'Home', path: '/index'})
 }
+
+exports.getOrders = (req, res, next)=>{
+res.render('shop/orders', {pageTitle: 'Your Orders', path: '/orders'})
+}
+
--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,14 @@ const router = express.Router();
 
 const shopController = require('../controllers/shop')
 
-router.get('/', shopController.productsData);
+router.get('/index', shopController.productsData);
 
 router.get('/checkout', shopController.getCheckout);
 
 router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/products-list', shopController.productsData)
 
 module.exports = router;
