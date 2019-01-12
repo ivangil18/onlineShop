26c934a456e4dd3108173507b618d23ce93bb7a6
Ivan Gil <igil.dev@gmail.com>
2019-01-12 18:37:19 -0500

    dynamic pagination added

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const PDFdocument = require('pdfkit');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.productsData = (req, res, next) => {
   Product.find()
@@ -105,15 +105,28 @@ exports.getCheckout = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  const page = +req.query.page || 1 ;
+  let totalItems;
+
   Product.find()
-    .skip((page - 1) * ITEMS_PER_PAGE)
-    .limit(ITEMS_PER_PAGE)
+    .countDocuments()
+    .then(countProd => {
+      totalItems = countProd;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/index'
+        path: '/index',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
